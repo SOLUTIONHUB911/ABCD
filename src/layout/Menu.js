5b0c5aa9c2cf040a2021a5db4ae98aaf12b417c6
solutionhub911 <solutionhub911@gmail.com>
2022-06-26 22:41:45 +0100

    fixed: error with icon

--- src/layout/Menu.js
+++ src/layout/Menu.js
@@ -4,30 +4,46 @@ import { useRouter } from 'next/router';
 export default function Menu() {
 	const router = useRouter();
 	return (
-		<nav className="border bg-[#121212] absolute z-10 top-[80px] left-[65%]">
+		<nav className='border bg-[#121212] absolute z-10 top-[80px] left-[65%]'>
 			<ul>
-				<Link href="/About">
-					<a className={router.pathname == '/About' ? 'text-[#ffd000]' : 'text-white'}>
-						<li className="p-4 border-b-red">About</li>
+				<Link href='/About'>
+					<a
+						className={`${
+							router.pathname == '/About' ? 'text-[#ffd000]' : 'text-white'
+						} font-roboto font-bold text-[20px] leading-[28px]`}
+					>
+						<li className='p-4 border-b-red'>About</li>
 					</a>
 				</Link>
-				<Link href="/Blog">
-					<a className={router.pathname == '/Blog' ? 'text-[#ffd000]' : 'text-white'}>
-						<li className="p-4">Blog</li>
+				<Link href='/Blog'>
+					<a
+						className={`${
+							router.pathname == '/Blog' ? 'text-[#ffd000]' : 'text-white'
+						} font-roboto font-bold text-[20px] leading-[28px]`}
+					>
+						<li className='p-4'>Blog</li>
 					</a>
 				</Link>
-				<Link href="/Community">
-					<a className={router.pathname == '/Community' ? 'text-[#ffd000]' : 'text-white'}>
-						<li className="p-4">Community</li>
+				<Link href='/Community'>
+					<a
+						className={`${
+							router.pathname == '/Community' ? 'text-[#ffd000]' : 'text-white'
+						} font-roboto font-bold text-[20px] leading-[28px]`}
+					>
+						<li className='p-4'>Community</li>
 					</a>
 				</Link>
-				<Link href="/Contact">
-					<a className={router.pathname == '/Contact' ? 'text-[#ffd000]' : 'text-white'}>
-						<li className="p-4">Contact us</li>
+				<Link href='/Contact'>
+					<a
+						className={`${
+							router.pathname == '/Contact' ? 'text-[#ffd000]' : 'text-white'
+						} font-roboto font-bold text-[20px] leading-[28px]`}
+					>
+						<li className='p-4'>Contact us</li>
 					</a>
 				</Link>
 			</ul>
-			<button className="hidden lg:block bg-[#FFD000] py-2 px-5 text-black font-bold text-base rounded w-[100px] h-[40px]">
+			<button className='hidden lg:block bg-[#FFD000] py-2 px-5 text-black font-bold text-base rounded w-[100px] h-[40px]'>
 				Join ABCD
 			</button>
 		</nav>
--- src/pages/FAQ.js
+++ src/pages/FAQ.js
@@ -4,26 +4,21 @@ import Navigation from '../components/Navigation';
 import styles from '/styles/Hero.module.css';
 export default function FAQ() {
 	return (
-		<div>
-			<Navigation />
-			<h2 className="w-[35%] font-bold text-[30px] lg:text-[50px] text-[#ffd000] mt-20 mx-[25px] lg:mx-[50px]">
+		<div className='py-8'>
+			<h2 className='w-[35%] font-bold text-[30px] lg:text-[50px] text-[#ffd000] mt-20 mx-[25px] lg:mx-[50px]'>
 				FAQs
 			</h2>
-			<h2 className="w-[35%] font-bold text-[30px] lg:text-[50px] text-[#ffd000] mt-20 mx-[25px] lg:mx-[50px]">
+			<h2 className='w-[35%] font-bold text-[30px] lg:text-[50px] text-[#ffd000] mt-20 mx-[25px] lg:mx-[50px]'>
 				ABCD Talent Vetting & Quality
 			</h2>
 
 			<div className={styles.preferences}>
-				<Collapsible title="DATA COLLECTION" defaultExpanded="true">
+				<Collapsible title='DATA COLLECTION' defaultExpanded='true'>
 					<p>Here we collect data from the devs to understand their background and strength.</p>
 				</Collapsible>
-				<Collapsible title="FIRST INTERVIEW" defaultExpanded="false" />
-				<Collapsible title="TECHNICAL INTERVIEW" defaultExpanded="false" />
-				<Collapsible title="FINAL STAGE & COMPLETE PROFILING" defaultExpanded="false" />
-			</div>
-
-			<div className="mt-[35rem]">
-				<Footer />
+				<Collapsible title='FIRST INTERVIEW' defaultExpanded='false' />
+				<Collapsible title='TECHNICAL INTERVIEW' defaultExpanded='false' />
+				<Collapsible title='FINAL STAGE & COMPLETE PROFILING' defaultExpanded='false' />
 			</div>
 		</div>
 	);
